bef313058288c977518ddeac98323923d2f2da03
mokhtarjo21 <mokhtar.jo21@gmail.com>
2026-02-12 16:24:04 +0200

    add host

--- smart_teacher_assistant/settings.py
+++ smart_teacher_assistant/settings.py
@@ -13,8 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
-
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOED_HOSTS =['mekhotek.pythonanywhere.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
